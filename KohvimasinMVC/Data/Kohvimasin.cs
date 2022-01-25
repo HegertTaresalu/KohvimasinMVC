cd26e82c202984d42b4c6637bcc718c8b38d7bf6
opilane <opilane@HP-CZC6297B8B>
2022-01-25 13:47:50 +0200

    Create model

---
+++ KohvimasinMVC/Data/Kohvimasin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KohvimasinMVC.Data
+{
+    public class Kohvimasin
+    {
+        public int Id { get; set; }
+        public string Jooginimi { get; set; }
+        public int Topsepakis { get; set; }
+        public int Topsejuua { get; set; }
+    }
+}
